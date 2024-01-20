c68aee9d561dfc4ca6ed68bf30077319d708fbe9
Rafael Silva <nightmarezlolzin@gmail.com>
2024-01-20 00:33:33 -0300

    Construção da estrutura MVC com Flask

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='')
+app = Flask(__name__)
+app.secret_key = ''
+
+login_manager = login_manager(app)
+login_manager.login.view = 'pagina_entrar'
+
+class User(UserMixin):
+        pass
+
+def load_user(user_id):
+        
+        
+        pass
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/paginaCursos')
+def pagina_cursos():
+        return render_template('paginaCursos.html')
+
+
+@app.route('/paginaEntrar')
+def pagina_entrar():
+        return render_template('paginaEntrar.html')
+
+@app.route('/paginaQuiz')
+def paginha_quiz():
+        return render_template('paginaQuiz.html')
+
+@app.route('/paginaRegistro')
+def pagina_registro():
+        return render_template('paginaRegistro.html')
+
+@app.route('/paginaResultado')
+def pagina_resultado():
+        return render_template('paginaResultado.html')
+
+@app.route('/paginaSobre')
+def pagina_sobre():
+    return render_template('paginaSobre.html')
+
+@app.route('/paginaUsuario')    
+def pagina_usuario():
+        return render_template('paginaUsuario.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True) 
+    
+
     
